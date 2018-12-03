c641925097355177409908e70e5bc5c1001ae778
Nate Gamble <neg6@calvin.edu>
2018-12-03 14:25:39 -0500

    added an eval function

--- Project05/MancalaNate/MancalaNate/skeletronPrime.cs
+++ Project05/MancalaNate/MancalaNate/skeletronPrime.cs
@@ -35,13 +35,13 @@ namespace Mankalah
             do
             {
                 int[] result = minmaxVal(b, depth, float.MinValue, float.MaxValue, ref sw);
-            if (result[1] > moveToTake[1])
-                moveToTake = result;
-            Console.WriteLine($"Depth{depth}: best move is {moveToTake[0]}");
-            depth++;
-        } while (sw.ElapsedMilliseconds<(timeLimit - 500));
+                if (result[1] > moveToTake[1])
+                    moveToTake = result;
+                Console.WriteLine($"Depth{depth}: best move is {moveToTake[0]} with score {moveToTake[1]}");
+                depth++;
+            } while (sw.ElapsedMilliseconds<(timeLimit - 100) && depth <=50);
 
-            Console.WriteLine($"Elapsed Time: {sw.ElapsedMilliseconds}, time allowed: {timeLimit}");
+                Console.WriteLine($"Elapsed Time: {sw.ElapsedMilliseconds}, time allowed: {timeLimit}");
 
             return moveToTake[0];
 
@@ -51,8 +51,10 @@ namespace Mankalah
 
         public int[] minmaxVal(Board b, int depth, float alpha, float beta, ref Stopwatch sw)
         {
-            if (depth == 0 || b.gameOver())
+            if (depth == 0)
                 return new int[] { 0, evaluate(b) };
+            if (b.gameOver())
+                return new int[] { 0, endGameEval(b) };
             int[] bestMove = { 0, int.MinValue };
 
             //our turn -> maximize
@@ -63,8 +65,9 @@ namespace Mankalah
                 {
                     if (b.legalMove(i))
                     {
-                        if (sw.ElapsedMilliseconds >= timeLimit - 500)
-                            return new int[] { 0, int.MinValue };
+                        if (sw.ElapsedMilliseconds >= timeLimit - 100)
+                            //return new int[] { 0, int.MinValue };
+                            return new int[] { 0, evaluate(b) };
                         Board modified = SimulateBoard(b, i);
 
                         int[] newMove = minmaxVal(modified, depth - 1, alpha, beta, ref sw);
@@ -89,8 +92,9 @@ namespace Mankalah
                 {
                     if (b.legalMove(i))
                     {
-                        if (sw.ElapsedMilliseconds >= timeLimit - 500)
-                            return new int[] { 0, int.MaxValue };
+                        if (sw.ElapsedMilliseconds >= timeLimit - 100)
+                            //return new int[] { 0, int.MaxValue };
+                            return new int[] { 0, evaluate(b) };
 
                         Board modified = SimulateBoard(b, i);
 
@@ -117,9 +121,44 @@ namespace Mankalah
             return modified;
         }
 
+        /*Evaluate function used when the end game is not clear
+         * Uses 5 heuristics found by researchers at the University of Kansas to be the best
+         * Heuristic research found at https://fiasco.ittc.ku.edu/publications/documents/Gifford_ITTC-FY2009-TR-03050-03.pdf
+         * h1 is my score - opponent's score
+         * h2 is how close I am to winning
+         * h3 is how close the opponent is to winning
+         * h4 is the number of stones close to my home
+         * h5 is the number of stones far from my home (on my side of the board)
+         */
         public override int evaluate(Board b)
         {
-            int h1 = 
+            int h1, h2, h3, h4, h5, sum;
+            if (us == Position.Top)
+            {
+                h2 = b.scoreTop();
+                h3 = b.scoreBot();
+                h1 = h2 - h3;
+                h4 = b.stonesAt(11) + b.stonesAt(12);
+                h5 = b.stonesAt(7) + b.stonesAt(8);
+            }
+            else
+            {
+                h2 = b.scoreBot();
+                h3 = b.scoreTop();
+                h1 = h2 - h3;
+                h4 = b.stonesAt(4) + b.stonesAt(5);
+                h5 = b.stonesAt(0) + b.stonesAt(1);
+            }
+            sum = h1 + h2 - h3 - h4 + h5;
+            return sum;
+        }
+
+        public int endGameEval(Board b)
+        {
+            if (us == Position.Top)
+                return (b.scoreTop() - b.scoreBot()) * 1000;
+            else
+                return (b.scoreBot() - b.scoreTop()) * 1000;
         }
 
     }
